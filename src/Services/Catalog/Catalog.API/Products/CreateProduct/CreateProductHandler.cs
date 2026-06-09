d1152e84e068ffadfcd3e1c34e5b62177891511d
tam731 <levantam4199@gmail.com>
2026-06-09 17:27:49 +0700

    added BuildingBlock project, cfg Catalog.API project and test creat product

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,34 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand
+(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price
+):ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler 
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product=new Product
+        {
+           // Id = Guid.NewGuid(),
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        return Task.FromResult(new CreateProductResult(Guid.NewGuid()));
+    }
+}
